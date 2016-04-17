1d92b2086de1285e9eb432daccf6ba473d377e5f
郭鹏 <532957426@qq.com>
2016-04-18 00:31:31 +0800

    consumer producer

--- generator.py
+++ generator.py
@@ -26,3 +26,26 @@ def fib_g(max):
 
 for x in fib_g(10):
     print x
+
+
+def consumer():
+    n = 0
+    while True:
+        n = yield n
+        n -= 1
+        print 'consume 1 ,left %d' % n
+
+
+def producer(c):
+    n = 0
+    c.next()
+    while n < 10:
+        n += 2
+        print 'produce 2 ,left %d' % n
+        n = c.send(n)
+        print 'confirm %d' % n
+    c.close()
+
+
+c = consumer()
+producer(c)
