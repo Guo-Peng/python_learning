e4ba8968389efb84cd5a79531ed316b0b6a2ffa5
郭鹏 <532957426@qq.com>
2016-04-18 00:33:04 +0800

    file change

--- coroutine.py
+++ coroutine.py
@@ -1,1 +1,22 @@
 # -*- coding: UTF-8 -*-
+def consumer():
+    n = 0
+    while True:
+        n = yield n
+        n -= 1
+        print 'consume 1 ,left %d' % n
+
+
+def producer(c):
+    n = 0
+    c.next()
+    while n < 10:
+        n += 2
+        print 'produce 2 ,left %d' % n
+        n = c.send(n)
+        print 'confirm %d' % n
+    c.close()
+
+
+c = consumer()
+producer(c)
