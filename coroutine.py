b20d601fbc38fa5c970c51c8c06e2cdf4918a13d
guopeng <guopeng@gitlab.local>
2016-04-20 22:33:32 +0800

    pool

--- coroutine.py
+++ coroutine.py
@@ -48,7 +48,49 @@ from multiprocessing.dummy import Pool as ThreadPool
 #     gevent.spawn(f1),
 #     gevent.spawn(f2)
 # ])
+#
+#
+# start = time.time()
+# during = lambda: time.time() - start
+#
+#
+# def fetch(n):
+#     s = time.time()
+#     requests.get('http://www.baidu.com')
+#     print 'process %d : %1.2fs -- %1.2fs' % (n, time.time() - s, during())
+#
+#
+# def synchronous():
+#     for x in xrange(10):
+#         fetch(x)
+#
+#
+# def thread():
+#     pool = ThreadPool(10)
+#     for x in xrange(100):
+#         pool.apply_async(fetch, args=(x,))
+#     pool.close()
+#     pool.join()
+#
+#
+# def asynchronous():
+#     gevent.joinall([gevent.spawn(fetch, x) for x in xrange(100)])
+#
+#
+# start = time.time()
+# print 'synchronous'
+# synchronous()
+# print 'during %1.2f ' % during()
+# start = time.time()
+# print 'thread'
+# thread()
+# print 'during %1.2f ' % during()
+# start = time.time()
+# print 'asynchronous'
+# asynchronous()
+# print 'during %1.2f ' % during()
 
+from gevent.pool import Pool
 
 start = time.time()
 during = lambda: time.time() - start
@@ -57,35 +99,12 @@ during = lambda: time.time() - start
 def fetch(n):
     s = time.time()
     requests.get('http://www.baidu.com')
-    print 'process %d : %1.2fs -- %1.2fs' % (n, time.time() - s, during())
+    print 'process %d : %1.2fs -- %s' % (n, time.time() - s, during())
 
 
-def synchronous():
-    for x in xrange(10):
-        fetch(x)
-
-
-def thread():
-    pool = ThreadPool(10)
-    for x in xrange(100):
-        pool.apply_async(fetch, args=(x,))
-    pool.close()
-    pool.join()
-
-
-def asynchronous():
-    gevent.joinall([gevent.spawn(fetch, x) for x in xrange(100)])
-
-
-# start = time.time()
-# print 'synchronous'
-# synchronous()
-# print 'during %1.2f ' % during()
-start = time.time()
-print 'thread'
-thread()
-print 'during %1.2f ' % during()
-start = time.time()
-print 'asynchronous'
-asynchronous()
-print 'during %1.2f ' % during()
+pool = Pool(5)
+pool.map(fetch, xrange(10))
+# 是否需要等待结束
+# pool.join()
+print 'test'
+print during()
